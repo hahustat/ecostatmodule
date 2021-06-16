99f7e02b5d94bdbe7a5a446ac0f099a4c0cd98ea
Tedros Gebregergs Welday <70442463+hahustat@users.noreply.github.com>
2021-06-16 11:39:52 +0200

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,165 @@
+#' @importFrom maptools readShapePoly
+NULL
+
+#' @importFrom rgdal readOGR
+NULL
+
+#'  Wine.
+#'
+#' Data on some characteristics of the wine industry in Australia, a dataset
+#' for simultaneous equation Models(SEM) in Econometrics_Stat3061 module.
+#'
+#' @format A data frame with seven variables:
+#' \describe{
+#' \item{\code{year}}{age in years}
+#' \item{\code{consumption}}{real per capita consumption of wine}
+#' \item{\code{storage_costs}}{index of storage costs}
+#' \item{\code{price_wine}}{ price of wine relative to CPI}
+#' \item{\code{price_beer}}{price of beer relative to CPI}
+#' \item{\code{advertising}}{real per capita advertising expenditure}
+#' \item{\code{income}}{real per capita disposable income}}
+"wine"
+
+
+
+#' House_price.
+#'
+#' Data on some characteristics of the wine industry in Australia, a dataset
+#' for simultaneous equation Models(SEM) in Econometrics_Stat3061 module.
+#'
+#' @format A data frame with seven variables:
+#' \describe{
+#' \item{\code{price }}{  house price, in \$1000}
+#' \item{\code{sqft}}{ square feet of living area, in 100s}
+#' \item{\code{age}}{ house age, in years}
+#' \item{\code{utown}}{  =1 if close to university}
+#' \item{\code{pool}}{ =1 if house has pool}
+#' \item{\code{fplace}}{=1 if house has fireplace}}
+"house_price"
+
+
+
+#' educ_inc.
+#'
+#' Wage  data,  a dataset in Econometrics_Stat3061 module.
+#'
+#' @format A data frame with six variables:
+#' \describe{
+#' \item{\code{faminc}}{ Family income in 2006 dollars}
+#' \item{\code{hedu}}{ Husband's educational attainment, in years}
+#' \item{\code{wedu}}{ Wife's educational attainment, in years}
+#' \item{\code{k16}}{  Number of children less than 6 years old in household}
+#' \item{\code{x5}}{x5 is an artificially generated variable}
+#' \item{\code{x6}}{ x6 is an artificially generated variable} }
+"educ_inc"
+
+
+#' cps2.
+#'
+#' wage dataset in Econometrics_Stat3061 module.
+#'
+#' @format A data frame with ten variables:
+#' \describe{
+#' \item{\code{wage}}{  earnings per hour }
+#' \item{\code{educ}}{  years of education}
+#' \item{\code{exper}}{ experience }
+#' \item{\code{female}}{  =1 if female }
+#' \item{\code{black}}{  =1 if black }
+#' \item{\code{married}}{  =1 if married }
+#' \item{\code{union}}{  =1 if union member }
+#' \item{\code{south}}{  =1 if south }
+#' \item{\code{fulltime}}{  =1 if full time worker }
+#' \item{\code{metro}}{  =1 if lives in metropolitan area}}
+"cps2"
+
+
+#' Auto.
+#'
+#'  Data on investment and value of outstanding shares for the years 1935-1953
+#'  in Econometrics_Stat3061 module.
+#'
+#' @format A data frame with 3 variables:
+#' \describe{
+#' \item{\code{year}}{ household number }
+#' \item{\code{invest}}{ investment}
+#' \item{\code{vsh}}{ value of outstanding shares } }
+"auto"
+
+
+
+#' Import.
+#'
+#'  Data set of imports in Econometrics_Stat3061 module.
+#'
+#'  @format A data frame with five variables: \code{year}, \code{imports},
+#'   \code{GDP}, \code{stock.formation} and \code{consumption}
+"import"
+
+
+
+#' Consmption.
+#'
+#' Consmption dataset in Econometrics_Stat3061 module.
+#'
+#' @format A data frame with 3 variables:
+#' \describe{
+#' \item{\code{household}}{ household number }
+#' \item{\code{income}}{ household income }
+#' \item{\code{expen}}{ household consumption expenditure } }
+"consmption"
+
+
+
+
+
+#' Demo2.
+#'
+#' hypothetical  data,  a dataset in Econometrics_Stat3061 module.
+#'
+#' @format A data frame with 2 variables:
+#' \describe{
+#' \item{\code{x}}{ weekly income }
+#' \item{\code{y}}{ weekly consumption expenditure } }
+"demo2"
+
+#' ele_cons.
+#'
+#'data on the percentage rate of change in electricity consumption (millions KWH)
+#'(Y) and the rate of change in the price of electricity (Birr/KWH) (X) for the
+#'years 1979 – 1994,  a dataset in Econometrics_Stat3061 module.
+#'
+#'@format A data frame with three variables: \code{year}, \code{X}, \code{Y}
+"ele_cons"
+
+
+#' Food_cons
+#'
+#'data on per capita food consumption (Y), price of food (X2 ) and
+#'per capita income (X3) where retail price of food and per capita disposable income are deflated by
+#'the Consumer Price Index.
+#'
+#'@format A data frame with four variables: \code{Year}, \code{Y},  \code{X2} and \code{X3}
+"food_cons"
+
+
+#' Food
+#'
+#'Dataset in  Econometrics_Stat3061 module.
+#'
+#'@format A data frame with two variables: \code{food_exp}, \code{income}
+"food"
+
+
+
+#' Saving data
+#'
+#' Data is on domestic household savings and gross domestic product  for India
+#' for the period 1980 to 2002, a dataset in Econometrics_Stat3061 module.
+#'
+#'@format A data frame with three variables: \code{year}, \code{saving},and \code{GDP}
+"savingdata"
+
+#' Crime_A
+#' Spatial data of about crime distribution of Addis Ababa city, Ethiopia.
+#'
+
---
+++ README.md
@@ -0,0 +1,49 @@
+# econstatmodule
+
+
+
+This package contains 15 datasets of Econometrics_stat3061 module:
+
+* `auto`
+
+* `consmption`
+
+* `cps2`
+
+* `crime_A`
+
+* `demo1`
+
+* `demo2`
+
+* `educ_inc`
+
+* `ele_cons`
+
+* `food`
+
+* `food_cons`
+
+* `house_price`
+
+* `import`
+
+* `savingdata`
+
+* `wine`
+
+
+
+## Installation
+
+```R
+# Install the released version from CRAN
+install.packages("ecostatmodule")
+
+# Install the development version from GitHub
+devtools::install_github("hahustat/ecostatmodule")
+```
+
+-----
+
+Please note that the 'babynames' project is released with a [Contributor Code of Conduct](.github/CODE_OF_CONDUCT.md). By contributing to this project, you agree to abide by its terms.
